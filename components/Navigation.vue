400604da9f997650065c8ecca3907a067d00a887
Daniel Rötzer <danielroetzer@users.noreply.github.com>
2019-01-04 18:30:20 +0100

    created new component for Navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="character">
+    <div class="flex flex-wrap content-center items-center" style="height: 100%;">
+      <div class="w-1/2 md:w-full mb-4">
+        <img src="~assets/img/portrait-400x400.png" class="character-img">
+        <h2>Daniel Rötzer</h2>
+      </div>
+      <div class="w-1/2 md:w-full mb-4">Text about me</div>
+      <!--<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey">C</div>
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey-light">D</div>
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey">E</div>
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey-light">F</div>-->
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <div class="home">
-      <div class="character">
-        <div class="flex flex-wrap content-center items-center" style="height: 100%;">
-          <div class="w-1/2 md:w-full mb-4">
-            <img src="~assets/img/portrait-400x400.png" class="character-img">
-            <h2>Daniel Rötzer</h2>
-          </div>
-          <div class="w-1/2 md:w-full mb-4">Text about me</div>
-          <!--<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey">C</div>
-          <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey-light">D</div>
-          <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey">E</div>
-          <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey-light">F</div>-->
+  <div class="home">
+    <div class="character">
+      <div class="flex flex-wrap content-center items-center" style="height: 100%;">
+        <div class="w-1/2 md:w-full mb-4">
+          <img src="~assets/img/portrait-400x400.png" class="character-img">
+          <h2>Daniel Rötzer</h2>
         </div>
+        <div class="w-1/2 md:w-full mb-4">Text about me</div>
+        <!--<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey">C</div>
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-grey-light">D</div>
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey">E</div>
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-grey-light">F</div>-->
       </div>
-
-      <Timeline/>
     </div>
+
+    <Timeline/>
   </div>
 </template>
 
