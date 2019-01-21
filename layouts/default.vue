922818d166116a7ca8d5838aa6efc8b7fedcbcfe
Daniel Rötzer <d.roetzer@it-center.at>
2019-01-21 14:09:03 +0100

    reduced horizontal padding on page-container for smaller widths

--- layouts/default.vue
+++ layouts/default.vue
@@ -62,7 +62,7 @@ export default {
 
 .page-container {
   width: 100%;
-  padding: 30px;
+  padding: 30px 20px;
 
   // Margin for Footer
   margin-bottom: 35px;
@@ -70,6 +70,7 @@ export default {
   @media screen and (min-width: $utility-breakpoint){
     margin-left: $utility-width;
     //max-width: $content-width;
+    padding: 30px;
   }
 
   .page-content {
